ddeeb6e68434a88988b87a48380e76bd64110af6
Francesco Romano <francesco.romano.1987@gmail.com>
2016-04-12 12:32:34 +0200

    parameters

--- src/modules/torqueBalancing/app/scripts/GenerationOfReferences/src/main.cpp
+++ src/modules/torqueBalancing/app/scripts/GenerationOfReferences/src/main.cpp
@@ -3,6 +3,7 @@
 #include <yarp/os/ResourceFinder.h>
 #include <yarp/os/Value.h>
 #include <yarp/os/LogStream.h>
+#include <yarp/sig/Vector.h>
 #include "OptimProblem.h"
 
 
@@ -32,14 +33,45 @@ int main(int argc, char **argv) {
     yInfo() << "Robot model found in " << filepath;
     
     //read desired CoM
+    if (!resourceFinder.check("comDes", "Checking desired CoM parameter")) {
+        yError("Parameter comDes is required");
+        return -1;
+    }
+    Value &comDes = resourceFinder.find("comDes");
+    //Start checking: it should be a list of 3
+    if (!comDes.isList() || comDes.asList()->size() != 3) {
+        yError("Number of elements in comDes parameter is wrong. Expecting 3 values");
+        return -2;
+    }
+    yarp::sig::Vector desiredCoM(3);
+    Bottle* comList = comDes.asList();
+    desiredCoM[0] = comList->get(0).asDouble();
+    desiredCoM[1] = comList->get(1).asDouble();
+    desiredCoM[2] = comList->get(2).asDouble();
+
     //read desired Joints configuration
     //(Which joints!? The model has more joints than we need. We have to map this stuff)
+    if (!resourceFinder.check("qDes", "Checking desired joint configuration parameter")) {
+        yError("Parameter qDes is required");
+        return -1;
+    }
+    Value &qDes = resourceFinder.find("qDes");
+
     //read which foot/feet is in support
+    if (!resourceFinder.check("feetInSupport", "Checking feet in support parameter")) {
+        yError("Parameter feetInSupport is required");
+        return -1;
+    }
+    Value &feet = resourceFinder.find("feetInSupport");
 
 
     OptimProblem problem;
-    problem.initializeModel(filepath);
+    if (!problem.initializeModel(filepath)) {
+        yError("Error initializing the robot model");
+        return -2;
+    }
 
+    problem.solveOptimization(desiredCoM, desiredCoM, feet.asString());
 
 
     return 0;
